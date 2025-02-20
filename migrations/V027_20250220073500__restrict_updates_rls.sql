cdf727d82e58d7824c960d0c0ac12b75e5ae1a70
Katlego Sekoele <katlego.sekoele@bbd.co.za>
2025-02-20 13:19:23 +0200

    updated rls

--- migrations/V027_20250220073500__restrict_updates_rls.sql
+++ migrations/V027_20250220073500__restrict_updates_rls.sql
@@ -4,6 +4,16 @@ In our case, a tenant will be a shelter?
 So rather use => SET app.current_shelter = <shelter-id>;
 */
 
+-- DROP PREVIOUS POLICIES
+
+DROP POLICY IF EXISTS ShelterMissionsIsolationPolicy ON "BeanMissions";
+DROP POLICY IF EXISTS ShelterMissionYieldIsolationPolicy ON "MissionYieldItems";
+DROP POLICY IF EXISTS ShelterBeanSupplyIsolationPolicy ON "BeanSupplies";
+DROP POLICY IF EXISTS ShelterResidentsIsolationPolicy ON "ShelterSurvivors";
+DROP POLICY IF EXISTS ShelterSurvivorIsolationPolicy ON "Survivors";
+DROP POLICY IF EXISTS ShelterBeanRequestsIsolationPolicy ON "BeanRequests";
+
+
 CREATE OR REPLACE FUNCTION getAppCurrentShelter() RETURNS INTEGER AS $$
 DECLARE 
     ShelterId TEXT;
@@ -23,8 +33,9 @@ $$ LANGUAGE plpgsql;
 ALTER TABLE "BeanMissions"
 ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS shelter_missions_isolation_policy ON "BeanMissions";
+DROP POLICY IF EXISTS ShelterMissionsIsolationPolicy ON "BeanMissions";
 CREATE POLICY ShelterMissionsIsolationPolicy ON "BeanMissions"
+FOR ALL
 USING (
   "ShelterID" = getAppCurrentShelter()
 )
@@ -35,8 +46,9 @@ WITH CHECK (
 ALTER TABLE "MissionYieldItems"
 ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS shelter_mission_yield_isolation_policy ON "MissionYieldItems";
+DROP POLICY IF EXISTS ShelterMissionYieldIsolationPolicy ON "MissionYieldItems";
 CREATE POLICY ShelterMissionYieldIsolationPolicy ON "MissionYieldItems"
+FOR ALL
 USING (
   EXISTS (
         SELECT 1 
@@ -57,8 +69,9 @@ WITH CHECK (
 ALTER TABLE "BeanSupplies"
 ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS shelter_bean_supply_isolation_policy ON "BeanSupplies";
+DROP POLICY IF EXISTS ShelterBeanSupplyIsolationPolicy ON "BeanSupplies";
 CREATE POLICY ShelterBeanSupplyIsolationPolicy ON "BeanSupplies"
+FOR ALL
 USING (
   "ShelterID" = getAppCurrentShelter()
 )
@@ -69,8 +82,9 @@ WITH CHECK (
 ALTER TABLE "ShelterSurvivors"
 ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS shelter_residents_isolation_policy ON "ShelterSurvivors";
+DROP POLICY IF EXISTS ShelterResidentsIsolationPolicy ON "ShelterSurvivors";
 CREATE POLICY ShelterResidentsIsolationPolicy on "ShelterSurvivors"
+FOR ALL
 USING (
   "ShelterID" = getAppCurrentShelter()
 )
@@ -81,8 +95,9 @@ WITH CHECK (
 ALTER TABLE "Survivors"
 ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS shelter_survivor_isolation_policy ON "Survivors";
+DROP POLICY IF EXISTS ShelterSurvivorIsolationPolicy ON "Survivors";
 CREATE POLICY ShelterSurvivorIsolationPolicy on "Survivors"
+FOR ALL
 USING (
   EXISTS (
     SELECT 1
@@ -103,8 +118,9 @@ WITH CHECK (
 ALTER TABLE "BeanRequests"
 ENABLE ROW LEVEL SECURITY;
 
-DROP POLICY IF EXISTS shelter_bean_requests_isolation_policy ON "BeanRequests";
+DROP POLICY IF EXISTS ShelterBeanRequestsIsolationPolicy ON "BeanRequests";
 CREATE POLICY ShelterBeanRequestsIsolationPolicy on "BeanRequests"
+FOR ALL
 USING (
   EXISTS (
     SELECT 1
@@ -119,5 +135,117 @@ WITH CHECK (
     FROM "Survivors" s INNER JOIN "ShelterSurvivors" ss ON s."SurvivorID" = ss."SurvivorID"
     WHERE ss."SurvivorID" = "BeanRequests"."SurvivorID"
     AND ss."ShelterID" = getAppCurrentShelter()
+  )
+);
+
+ALTER TABLE "Shelters"
+ENABLE ROW LEVEL SECURITY;
+
+DROP POLICY IF EXISTS ShelterIsolationPolicy ON "Shelters";
+
+
+DROP POLICY IF EXISTS ShelterIsolationPolicy_Select ON "Shelters";
+CREATE POLICY ShelterIsolationPolicy_Select ON "Shelters"
+FOR SELECT
+USING (
+  true = true
+);
+
+DROP POLICY IF EXISTS ShelterIsolationPolicy_Insert ON "Shelters";
+CREATE POLICY ShelterIsolationPolicy_Insert ON "Shelters"
+FOR INSERT
+WITH CHECK (
+  "ShelterID" = getAppCurrentShelter()
+);
+
+DROP POLICY IF EXISTS ShelterIsolationPolicy_Delete ON "Shelters";
+CREATE POLICY ShelterIsolationPolicy_Delete ON "Shelters"
+FOR DELETE
+USING (
+  "ShelterID" = getAppCurrentShelter()
+);
+
+DROP POLICY IF EXISTS ShelterIsolationPolicy_Update ON "Shelters";
+CREATE POLICY ShelterIsolationPolicy_Update ON "Shelters"
+FOR UPDATE
+USING (
+  "ShelterID" = getAppCurrentShelter()
+)
+WITH CHECK (
+  "ShelterID" = getAppCurrentShelter()
+);;
+
+ALTER TABLE "BeanRequestItems"
+ENABLE ROW LEVEL SECURITY;
+
+DROP POLICY IF EXISTS ShelterRequestItemsIsolationPolicy ON "BeanRequestItems";
+CREATE POLICY ShelterRequestItemsIsolationPolicy ON "BeanRequestItems"
+FOR ALL
+USING (
+  EXISTS (
+    SELECT 1 
+    FROM "BeanRequestItems" bri 
+    INNER JOIN "BeanRequests" br ON br."BeanRequestID" = bri."BeanRequestID" 
+    INNER JOIN "Survivors" s ON br."SurvivorID" = s."SurvivorID"
+    INNER JOIN "ShelterSurvivors" ss ON ss."SurvivorID" = s."SurvivorID" 
+    WHERE ss."ShelterID" = getAppCurrentShelter()
+  )
+)
+WITH CHECK (
+  EXISTS (
+    SELECT 1 
+    FROM "BeanRequestItems" bri 
+    INNER JOIN "BeanRequests" br ON br."BeanRequestID" = bri."BeanRequestID" 
+    INNER JOIN "Survivors" s ON br."SurvivorID" = s."SurvivorID"
+    INNER JOIN "ShelterSurvivors" ss ON ss."SurvivorID" = s."SurvivorID" 
+    WHERE ss."ShelterID" = getAppCurrentShelter()
+  )
+);
+
+ALTER TABLE "Managers"
+ENABLE ROW LEVEL SECURITY;
+
+DROP POLICY IF EXISTS ManagerIsolationPolicy ON "Managers";
+DROP POLICY IF EXISTS ManagerIsolationPolicy_Update ON "Managers";
+CREATE POLICY ManagerIsolationPolicy_Update ON "Managers"
+FOR UPDATE
+USING (
+  EXISTS (
+    SELECT 1
+    FROM "Managers" m
+    INNER JOIN "ShelterSurvivors" ss ON m."SurvivorID" = ss."SurvivorID"
+    WHERE ss."ShelterID" = getAppCurrentShelter()
+  )
+)
+WITH CHECK (
+  EXISTS (
+    SELECT 1
+    FROM "Managers" m
+    INNER JOIN "ShelterSurvivors" ss ON m."SurvivorID" = ss."SurvivorID"
+    WHERE ss."ShelterID" = getAppCurrentShelter()
+  )
+);
+
+DROP POLICY IF EXISTS ManagerIsolationPolicy_Insert ON "Managers";
+CREATE POLICY ManagerIsolationPolicy_Insert ON "Managers"
+FOR INSERT
+WITH CHECK (
+  EXISTS (
+    SELECT 1
+    FROM "Managers" m
+    INNER JOIN "ShelterSurvivors" ss ON m."SurvivorID" = ss."SurvivorID"
+    WHERE ss."ShelterID" = getAppCurrentShelter()
+  )
+);
+
+DROP POLICY IF EXISTS ManagerIsolationPolicy_Delete ON "Managers";
+CREATE POLICY ManagerIsolationPolicy_Delete ON "Managers"
+FOR DELETE
+USING (
+  EXISTS (
+    SELECT 1
+    FROM "Managers" m
+    INNER JOIN "ShelterSurvivors" ss ON m."SurvivorID" = ss."SurvivorID"
+    WHERE ss."ShelterID" = getAppCurrentShelter()
   )
 );
